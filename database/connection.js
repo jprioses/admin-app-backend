6a5321279059bc2461edda1ff37161aef1b99d10
Juan Pablo Rios Escobar <jprioses@outlook.com>
2023-11-08 10:40:15 -0500

    Added index wich will be the entry ponit, also added connection and utils with response code

---
+++ database/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+//mongodb://localhost:27017/uran-app
+//mongodb+srv://jpriosescobar:1037634540@cluster0.fmjhghl.mongodb.net/uran-app
+
+const connection = async() => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/pemberty_adminApp');
+        console.log('Connected to database Pemberty aminApp');
+    } catch (error) {
+        console.log('here')
+        throw new Error(error);
+    }
+};
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,46 @@
+const connection = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
+
+const { errorResponse } = require("./utils/response");
+
+const path = require('path');
+
+console.log("Welcome to API Rest Permberty adminApp");
+
+connection();
+
+const app = express();
+const port = 3900;
+
+app.use(cors());
+
+//Parse body data from content-typ: application/json to json
+app.use(express.json());
+// Parse body data from form-url-encode data to json
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', express.static('client', {redirect: false}));
+
+//app.use("/api/credentials", CredentialsRoutes);
+
+
+//Redirect all routes differents to above ones
+app.get('*', function(req, res, next) {
+  return res.sendFile(path.resolve('client/index.html'));
+})
+
+//This middleware with error as parameter is the error handler function wich express is going to use
+app.use((err, req, res, next) => {
+  if (err.name == 'CastError') {
+    err.message = "Couldn't find document"
+    err.statusCode = 500
+  }
+  if (!err.statusCode) err.message = "Error while connecting to database";
+  errorResponse(res, err.statusCode || 500, err.message);
+});
+
+//Get server to listen
+app.listen(port, () => {
+  console.log("Server listening in port " + port);
+});
